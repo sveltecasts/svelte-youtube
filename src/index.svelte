10a6e271c8c5b70d8917f404ad5f86849e333c89
Sveltecasts <sveltecasts@gmail.com>
2019-07-13 17:32:53 +0200

    change name & component source

--- src/index.svelte
+++ src/index.svelte
@@ -1,35 +1,50 @@
-<script context="module">
-  // 2. This code loads the IFrame Player API code asynchronously.
-  var tag = document.createElement("script");
-
-  tag.src = "https://www.youtube.com/iframe_api";
-  var firstScriptTag = document.getElementsByTagName("script")[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-  window.onYouTubeIframeAPIReady = function() {
-    //console.log('hello')
-    window.dispatchEvent(new Event("YouTubeIframeAPIReady"))
-  }
-</script>
 
 <script>
   let player;
-  import { createEventDispatcher } from "svelte";
+  import { createEventDispatcher , onMount} from "svelte";
   const dispatch = createEventDispatcher();
   let divId = "player_"+parseInt(Math.random() * 100000).toString()
   export let videoId;
-  window.addEventListener("YouTubeIframeAPIReady", function(){
-      console.log('load player..')
-      player =  new YT.Player(divId, {
-          height: '390',
-          width: '640',
-          videoId: videoId,
-          events: {
-            //'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-          }
-        });
+  export let height = '390';
+  export let width = '640';
+  onMount(() => {
+      let ytTagUrl = "https://www.youtube.com/iframe_api";
+      if(!isMyScriptLoaded(ytTagUrl)){
+    // 2. This code loads the IFrame Player API code asynchronously.
+        var tag = document.createElement("script");
+        tag.src = ytTagUrl;
+        var firstScriptTag = document.getElementsByTagName("script")[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+       }
+      
+      window.onYouTubeIframeAPIReady = function() {
+        //console.log('hello')
+        window.dispatchEvent(new Event("YouTubeIframeAPIReady"))
+      }
+
+      window.addEventListener("YouTubeIframeAPIReady", function(){
+          console.log('load player..')
+          player =  new YT.Player(divId, {
+              height,
+              width,
+              videoId: videoId,
+              events: {
+                //'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+              }
+            });
+      })
+
   })
+
+function isMyScriptLoaded(url ="") {
+    var scripts = document.getElementsByTagName('script');
+    for (var i = scripts.length; i--;) {
+        if (scripts[i].src == url) return true;
+    }
+    return false;
+}
+
   function onPlayerStateChange({data}){
     dispatch("StateChange",data)
   }
@@ -37,5 +52,4 @@
     player.playVideo()
   }
 </script>
-YT-Component VID: {videoId}<br>
-<div id={divId}></div><br>
+<div id={divId}></div>
